9cac77850f809e0b3f0b9e1238cf69825d095bf4
Kobi Keren <kobikms728@outlook.com>
2016-10-21 12:13:33 +0300

    added the calculator functionality

--- CalculatorSLN/Calculator/Program.cs
+++ CalculatorSLN/Calculator/Program.cs
@@ -3,16 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationLib;
 
 namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Test");
+        {            
+            NumbersManager nm = new NumbersManager();
+            char selection;
+            int firstNumber, secondNumber;
+            do
+            {
+                Console.Write(
+                    "Please select [a]dd, [s]ubtract, [m]ultiply, [d]ivide, [e]xit: ");
+                selection = Console.ReadKey().KeyChar;
+                Console.WriteLine();
 
-            Console.ReadLine();
+                switch (selection)
+                {
+                    case 'a':
+                        firstNumber = nm.GetNumber("Please enter first number: ");
+                        secondNumber = nm.GetNumber("Please enter second number: ");
+                        Console.WriteLine("{0} + {1} = {2}",
+                            firstNumber, secondNumber, firstNumber + secondNumber);
+                        break;
+                    case 's':
+                        firstNumber = nm.GetNumber("Please enter first number: ");
+                        secondNumber = nm.GetNumber("Please enter second number: ");
+                        Console.WriteLine("{0} - {1} = {2}",
+                            firstNumber, secondNumber, firstNumber - secondNumber);
+                        break;
+                    case 'm':
+                        firstNumber = nm.GetNumber("Please enter first number: ");
+                        secondNumber = nm.GetNumber("Please enter second number: ");
+                        Console.WriteLine("{0} * {1} = {2}",
+                            firstNumber, secondNumber, firstNumber * secondNumber);
+                        break;
+                    case 'd':
+                        firstNumber = nm.GetNumber("Please enter first number: ");
+                        secondNumber = nm.GetNonZeroNumber("Please enter second number: ");
+                        Console.WriteLine("{0} / {1} = {2}",
+                            firstNumber, secondNumber, firstNumber / secondNumber);
+                        break;
+                    case 'e':
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            } while (selection != 'e');
+
+            Console.WriteLine();
+            Console.WriteLine("Goodbye....");
+
+
+
+            Console.ReadKey();
         }
     }
 }
